a2d3b907b350447dc691e611e35e4e3b6c20ee17
Isabelle <hatingusernames97@gmail.com>
2022-05-08 16:05:58 -0500

    added url and useState

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import{BrowserRouter, Routes, Route} from 'react-router-dom'
-import Home from './components/Home'
+import Home from './component/Home'
+import About from './component/About'
+import NotFound from './component/NotFound'
 
 function App() {
   return (
